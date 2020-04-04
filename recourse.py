62ae70fb7958b0c52dcbef4ef14bfca52d5b275b
b-low <bclow@umich.edu>
2020-04-03 21:16:51 -1000

    Code cleanup

--- recourse.py
+++ recourse.py
@@ -6,8 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_course(href):
-    course_id = parse.parse_qs(parse.urlsplit(href).query)["content"]
+def get_course(path):
+    course_id = parse.parse_qs(parse.urlsplit(path).query)["content"]
     return course_id[0][4:-3]
 
 
@@ -23,67 +23,59 @@ soup = BeautifulSoup(req.content, "html5lib")
 
 courses = {}
 
-flag = True
-while flag:
-    for div in soup.find_all("div", {"class": "ClassRow"}):
-        course = get_course(div["data-url"])
+while True:
+    for course_row in soup.find_all("div", {"class": "ClassRow"}):
+        course_path = course_row["data-url"]
+        course = get_course(course_path)
+
         if course not in courses:
-            course_url = "https://lsa.umich.edu/cg/" + div["data-url"]
+            course_url = "https://lsa.umich.edu/cg/" + course_path
+
+            print("Scraping ", course_url, "...", sep='')
+
             course_req = requests.get(course_url, headers)
             course_soup = BeautifulSoup(course_req.content, "html5lib")
 
-            print(course_url)
-
-
-            # # print(course_soup.select(".clsschedulerow > div > div:first-child"))
-            # for section in course_soup.select(".clsschedulerow > div > div:first-child"):
-            #     print(section)
-            #     if section.contents[1].string:
-            #         print(str(section.contents[1].string.encode('utf-8')))
-            #         # print({ "os" : section.contents[4].string, "ors" : section.contents[5].string, "wl" : section.contents[6].string})
-
+            # os = Open Seats, ors = Open Restricted Seats, wl = Wait List
             course_dict = {"os": 0, "ors": 0, "wl": 0}
 
             for section in course_soup.find_all("div", {"class": "clsschedulerow"}):
-                row = section.div.div
-                row_values = [x for x in row.stripped_strings]
+                section_data = [x for x in section.div.div.stripped_strings]
 
-
-                if str(row_values[3]) == "P":
+                # Only add the section if it's a primary section for enrollment
+                if str(section_data[3]) == "P":
                     last_value = ""
-                    for value in row_values:
-                        if last_value == "Open Seats:":
-                            if value.isnumeric():
+
+                    # section_data is in a flat list so refer to the previous element to determine the type of each value
+                    for value in section_data:
+                        # Add the section's data to the course's tota;
+                        if value.isnumeric():
+                            if last_value == "Open Seats:":
                                 course_dict["os"] += int(value)
-                        elif last_value == "Open Restricted Seats:":
-                            if value.isnumeric():
+                            elif last_value == "Open Restricted Seats:":
                                 course_dict["ors"] += int(value)
-                        elif last_value == "Wait List:":
-                            if value.isnumeric():
+                            elif last_value == "Wait List:":
                                 course_dict["wl"] += int(value)
                                 break
 
                         last_value = value
 
-                # if "LEC" in str(blah.div.div.div.span.string.encode('utf-8')):
-                #     print(blah)
-
             courses[course] = course_dict
-            print(course_dict)
-            # flag = False
-            # break
-    # break
+            print("Retrieved data for ", course, ": ", course_dict, sep='')
 
+    # Scrape all pages of the search results
+    # If there is no next page then exit the loop
     next_a = soup.find("a", {"id": "contentMain_hlnkNextTop"})
     if next_a is None:
         break
 
+    # Set the next page to be scraped to the next page of the search results
     next_req = requests.get("https://lsa.umich.edu/cg/" + next_a["href"], headers)
     soup = BeautifulSoup(next_req.content, "html5lib")
 
-print("Done scraping")
+print("Finished scraping ", len(courses), " courses")
 
+# Dump all course data to a json file for later use
 timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-
 with open(timestamp + ".json", "w") as fp:
     json.dump(courses, fp)
